4059682f00884002ea1a27463f640b14018d54ee
klajdi24 <klajdi2.bakiri@live.uwe.ac.uk>
2025-02-11 12:36:51 +0000

    works

---
+++ Assets/Scripts/interactions.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactions : MonoBehaviour
+{
+
+    bool annotationVisible = false; //current visibility of ship
+    public GameObject annotation; // annotation object
+
+    public void selected()
+    {
+
+        Debug.Log("show annotation");
+        //toggle visibility of the annotation
+        if (annotationVisible)
+        {
+            annotation.SetActive(false);
+            annotationVisible = false;
+        }else {
+            annotation.SetActive(true);
+            annotationVisible = true;
+        }
+
+    }
+}
